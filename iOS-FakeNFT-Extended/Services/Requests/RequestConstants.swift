f29853e0766ca2037d423d7d2b4c50a9347d1a6c
Lobov Vitaliy <lobov.vitaliy@gmail.com>
2025-06-24 08:21:16 +0300

    Update RequestConstants.swift -> add token

--- iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
@@ -1,5 +1,5 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
 #warning("insert your token here")
-    static let token = ""
+    static let token = "44bacb7b-39f3-4619-8f73-ae7078ff3670"
 }
