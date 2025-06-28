98fc983d565ca09d61775ea0b95f741702a20bd4
Demian Petropavlov <demyanpain@gmail.com>
2025-06-28 14:40:32 +0400

    [Feature] add struct CollectionDTO and NFTItem

--- iOS-FakeNFT-Extended/Models/Catalog/CollectionDTO.swift
+++ iOS-FakeNFT-Extended/Models/Catalog/CollectionDTO.swift
@@ -5,3 +5,12 @@
 //  Created by Vitaly Lobov on 26.06.2025.
 //
 
+struct CollectionDTO: Identifiable, Codable {
+    let id: String
+    let name: String
+    let cover: String
+    let description: String
+    let author: String
+    let nfts: [String]
+}
+
--- iOS-FakeNFT-Extended/Models/Catalog/NFTItem.swift
+++ iOS-FakeNFT-Extended/Models/Catalog/NFTItem.swift
@@ -5,3 +5,12 @@
 //  Created by Vitaly Lobov on 26.06.2025.
 //
 
+struct NFTItem: Identifiable, Codable {
+    let id: String
+    let name: String
+    let images: [String]
+    let rating: Int
+    let description: String
+    let price: Double
+    let author: String
+}
