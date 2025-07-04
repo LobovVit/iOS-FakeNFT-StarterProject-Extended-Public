9cbd2441477253f1a0a1951c3fe32af1f42ade48
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-07-04 20:40:14 +0300

    [Feature] add purchase block for PaymentView

--- iOS-FakeNFT-Extended/Screens/CartView/Components/PaymentView.swift
+++ iOS-FakeNFT-Extended/Screens/CartView/Components/PaymentView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 
 struct PaymentView: View {
+    private enum Constants {
+        static let buttonHeight: CGFloat = 60
+        static let buttonCornerRadius: CGFloat = 16
+        static let webViewURL: String = "https://practicum.yandex.ru/"
+    }
+    
     @ObservedObject var viewModel: CartViewModel
     
     let columns: [GridItem] = [
@@ -11,9 +17,12 @@ struct PaymentView: View {
     var body: some View {
         VStack {
             currenciesList
+            Spacer()
+            purchaseBlock
         }
         .navigationTitle(String(localized: "Select payment method"))
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar(.hidden, for: .tabBar)
     }
     
     private var currenciesList: some View {
@@ -29,6 +38,52 @@ struct PaymentView: View {
             }
             .padding(.vertical, 20)
             .padding(.horizontal, 16)
+        }
+    }
+    
+    private var purchaseBlock: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            textBlock
+            button
+        }
+        .padding(16)
+        .background(
+            Color.lightGreyDynamicYP
+                .cornerRadius(12, corners: [.topLeft, .topRight])
+                .ignoresSafeArea(.container, edges: .bottom)
+        )
+    }
+    
+    private var textBlock: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(String(localized: "By making a purchase, you agree to the terms of the"))
+                .font(Fonts.smallRegular)
+                .foregroundColor(.blackDynamicYP)
+            
+            NavigationLink {
+                WebView(url: URL(string: Constants.webViewURL))
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar(.hidden, for: .tabBar)
+                    .ignoresSafeArea(.container, edges: .bottom)
+            } label: {
+                Text(String(localized: "User agreement"))
+                    .font(Fonts.smallRegular)
+                    .foregroundColor(.blueUniversalYP)
+            }
+        }
+    }
+    
+    private var button: some View {
+        Button(action: {
+            // TODO: Действие при оплате
+        }) {
+            Text(String(localized: "Pay"))
+                .font(Fonts.bodyBold)
+                .foregroundColor(.whiteDynamicYP)
+                .frame(maxWidth: .infinity)
+                .frame(height: Constants.buttonHeight)
+                .background(.blackDynamicYP)
+                .cornerRadius(Constants.buttonCornerRadius)
         }
     }
 }
